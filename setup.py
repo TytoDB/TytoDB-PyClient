2abc7bec39f3bad4b4777ef6b2b404ee8ef9e5c5
theo <theo.dev.1234567890@gmail.com>
2025-05-10 21:57:31 -0300

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tytodb-pyclient",
-    version="0.1.5",
+    version="0.1.6",
     author="ttd3v",
     description="A TytoDB client for python",
     packages=find_packages(include=["tytodb_client", "tytodb_client.*"]),
